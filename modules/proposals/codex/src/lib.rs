bd72aaea33fa46d257d4beaf05c7770a04a6b67c
Shamil Gadelshin <shamilgadelshin@gmail.com>
2020-02-05 18:06:17 +0300

    Split proposals into two crates: engine and codex
    - split proposals into two crates: engine and codex
    - move engine and codex crates to the modules/proposals folder
    - place main modules code in lib.rs files
    - fix tests and dependencies

---
+++ modules/proposals/codex/src/lib.rs
@@ -0,0 +1,61 @@
+//! Proposals codex module for the Joystream platform. Version 2.
+//! Contains preset proposal types
+//!
+//! Supported extrinsics (proposal type):
+//! - create_text_proposal
+//!
+
+// Ensure we're `no_std` when compiling for Wasm.
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Do not delete! Cannot be uncommented by default, because of Parity decl_module! issue.
+//#![warn(missing_docs)]
+
+pub use proposal_types::{ProposalType, TextProposalExecutable};
+
+#[cfg(test)]
+mod tests;
+mod proposal_types;
+
+
+use proposal_engine::*;
+use rstd::vec::Vec;
+use codec::Encode;
+use rstd::clone::Clone;
+use srml_support::{decl_module, decl_storage};
+
+/// 'Proposals codex' substrate module Trait
+pub trait Trait: system::Trait + proposal_engine::Trait {}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as ProposalCodex {}
+}
+
+decl_module! {
+    /// 'Proposal codex' substrate module
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        /// Create text (signal) proposal type. On approval prints its content.
+        pub fn create_text_proposal(origin, title: Vec<u8>, body: Vec<u8>) {
+            let parameters = crate::ProposalParameters {
+                voting_period: T::BlockNumber::from(3u32),
+                approval_quorum_percentage: 49,
+            };
+
+            let text_proposal = TextProposalExecutable{
+                title: title.clone(),
+                 body: body.clone()
+               };
+            let proposal_code = text_proposal.encode();
+
+            <proposal_engine::Module<T>>::create_proposal(
+                origin,
+                parameters,
+                 title,
+                body,
+                text_proposal.proposal_type(),
+                proposal_code
+            )?;
+        }
+    }
+}
+
